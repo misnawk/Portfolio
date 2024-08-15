588bd0a86e3d42dd9b281313a8fb042f20c437f3
misnawk <kms13011@naver.com>
2024-08-15 18:42:54 +0900

    0815

--- src/Data.ts/ProfileData.ts
+++ src/Data.ts/ProfileData.ts
@@ -53,25 +53,25 @@ export const AboutMeData: AboutMeProps = {
   education: [
     {
       period: "2018.03 - 2024.02",
-      title: "네트워크 - 호원대학교",
+      title: "호원대학교 - 정보보안학과 졸업",
       description:
         "키 관리(키 분배, 키 생성, 키 저장), 대칭 암호(AES, DES), 비대칭 암호(RSA, ECC), 해시 함수(SHA-256), 디지털 서명(DSA), 스케줄링 알고리즘(FIFO, SJF, SRTF, RR 등), WireShark(패킷 분석 툴), Burp Suite(웹 애플리케이션 보안 테스트) 등을 학습했습니다. ",
     },
     {
       period: "2023.12.27 - 2024.01.31",
-      title: "자바(JAVA)특강 - 코리아IT아카데미 인천",
+      title: "자바(JAVA) - 코리아IT아카데미 인천",
       description:
         "자바의 기본 문법과 이클립스를 활용한 자바 프로그래밍을 다루었습니다.",
     },
     {
       period: "2024.01.10 - 2024.02.05",
-      title: "Mysql 사용한 DBMS특강 - 코리아IT아카데미 인천",
+      title: "Mysql 사용한 DBMS - 코리아IT아카데미 인천",
       description:
         "MySQL Workbench를 활용하여 SQL의 기본적인 CRUD와 JOIN을 통해 데이터를 제어해보았습니다",
     },
     {
       period: "2024.02.07 - 2024.07.16",
-      title: " 웹개발 특강 - 코리아IT아카데미 인천",
+      title: " 웹개발 - 코리아IT아카데미 인천",
       description:
         "프론트엔드에서는 HTML, CSS, JavaScript를 활용하고, 백엔드에서는 Spring, Spring Boot, Spring Tool Suite(STS)를 사용하여 MyBatis와 MySQL 데이터베이스를 효율적으로 제어하는 방법을 숙달했습니다.",
     },
@@ -171,7 +171,7 @@ export const SecondprojectData = {
 };
 
 export const thirdProjectData = {
-  title: "MY PROJECTS 3",
+  title: "To Be Continue",
   subtitle: " 프로젝트 이름: Project",
   description: "프로젝트 설명",
   features: ["주요기능"],
